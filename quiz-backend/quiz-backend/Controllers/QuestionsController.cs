55b7787e870d7ab7149a44a9a0445b5d0b317d29
Artem Pychenko <par@sitecore.net>
2019-12-08 15:03:38 +0800

    1. Created new backend project 2. Added couple of controllers to work with question entity

---
+++ quiz-backend/quiz-backend/Controllers/QuestionsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using quiz_backend.Models;
+
+namespace quiz_backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Questions")]
+    public class QuestionsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Models.Question> Get()
+        {
+            return new[]
+            {
+                new Question() { Text = "Hello" },
+                new Question() { Text = "Hi" },
+                new Question() { Text = "Greeting" }
+            };
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Models.Question question)
+        {
+        }
+    }
+}
