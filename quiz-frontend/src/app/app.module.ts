91a68b18f0ad7e6cfbefb196d08343aaf1661119
Artem Pychenko <par@sitecore.net>
2019-12-11 21:40:07 +0800

    Added angular routing for application. Implemented navigation bar.

--- quiz-frontend/src/app/app.module.ts
+++ quiz-frontend/src/app/app.module.ts
@@ -5,30 +5,46 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 import { ApiService } from './api.service';
 import { QuestionsComponent } from './questions.component';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './home.component';
+import { NavComponent } from './nav.component';
+
+const routes = [
+  { path: '', component: HomeComponent },
+  { path: 'question', component: QuestionComponent },
+  { path: 'questions', component: QuestionsComponent }
+]
 
 @NgModule({
    declarations: [
       AppComponent,
       QuestionComponent,
-      QuestionsComponent
+      QuestionsComponent,
+      HomeComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
-      AppRoutingModule,
+      RouterModule.forRoot(routes),
       BrowserAnimationsModule,
       MatSliderModule,
       MatInputModule,
       MatCardModule,
       FormsModule,
-      MatListModule
+      MatListModule,
+      MatToolbarModule,
+      MatButtonModule
+
    ],
    providers: [ApiService],
    bootstrap: [
